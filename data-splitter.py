29fdcdeb5ba04dca4d1393ce0f8edea06645fc15
Escape9002 <75317197+Escape9002@users.noreply.github.com>
2022-12-27 09:37:34 +0100

    split point reco works, no splitting of dataset now

--- data-splitter.py
+++ data-splitter.py
@@ -1,33 +1,77 @@
 import pandas as pd
 import numpy as np
+
+import matplotlib.pyplot as plt
+
 # validates wether the position of the splitting points actually correspond to the splitting points in the original dataset (csv_file)
-def val_split_pos(split = {}, csv_file = np.array, collumn = 0):
+def val_split_pos(split = [], np_array = np.array, collumn = 0):
     i=0
-    while(csv_file.size > i):
-        if ((i in split) and (np_array[collumn][i]) ):
+    for row,value in split:
+        if not (value == np_array[row][collumn]):
             return False
-        i += 1
     return True
 
-def val_multiple(split = np):
-    # if(split[1].isin[split]):
-    return 999
+# searches for multiple peaks at the same points to avoid too small splits
+def del_val_multiple(split):
+    single_vals = []
+    for i in range(len(split)-1):
+        if not (split[i][1] == split[(i+1)][1]):
+            single_vals.append(split[i][0])
+
+    return single_vals
+
+# splits the dataset at a givin value, no smoothening or anything, you have to applay del_va_multiple to have correct results
+def normal_splitting(collumn = 0, min_aX = -15.00, splits = [], np_array = np.array):
+    row = 0
+    while(len(np_array) > row):
+        if(np_array[row][collumn] < min_aX):
+            splits.append([row,np_array[row][collumn]])
+        row += 1
+
+    return del_val_multiple(splits)
+
+def split_data(np_array = np.array, splits = []):
+    # data_splitted = np.array([])
+    data_splitted = np.empty((len(np_array),3), dtype=float)
+    old = 0
+
+    for i in range(len(splits)):
+        tmp = np_array[old: splits[i]].copy()
+        print(tmp)
+        data_splitted = np.append(data_splitted, tmp , 0) 
+        
+        print("{} | {} ".format(old, splits[i]))
+        old = splits[i]
+        
+
+    return data_splitted
+
+
+##################################################
+##########THE REAL PROGRAMM ######################
+##################################################
 
 path =  'Data/2451_Shooting.csv'
 csv_file = pd.read_csv(path)
 np_array = csv_file.to_numpy()
+print(np_array)
 
-min_aX=-15.00
-row = 0
+min_aX = -15.00
 collumn = 0
-splits = {}
-# print(np_array[1][0])
+splits = []
+normalized_splits = []
 
-while(len(np_array) > row):
-    if(np_array[row][collumn] < min_aX):
-        splits[row] = np_array[row][collumn]
-    row += 1
-print("{},{},{}".format(splits , len(splits), csv_file.size))
-# print(splits[3360])
+normalized_splits = normal_splitting(collumn, min_aX, splits, np_array)
 
-# print('Values fit: {} | any Values found: {} | doubled Values: {}'.format(val_split_pos(splits, csv_file), splits.size, val_multiple()) )
+# print("all: {} \n normalized: {} ".format(splits, normalized_splits))
+print("\n##############################################################################\n")
+
+splitted_dataset = split_data(np_array, normalized_splits)
+
+print("0 ::: {}".format(splitted_dataset[0][0]))
+print("1 ::: {}".format(splitted_dataset[1][0]))
+
+#for i in range(len(splitted_dataset)):
+#    plt.plot(splitted_dataset[i])
+#    plt.ylabel("splitted_datasert {}".format(i))
+#    plt.show()

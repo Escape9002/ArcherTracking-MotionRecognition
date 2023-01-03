3e12a77b1b0506f4e1c75d72eeaa10da6d7c6dc9
Escape9002 <75317197+Escape9002@users.noreply.github.com>
2023-01-03 17:35:29 +0100

    dataset viewing via pyplot, file creation via csv, two splitted dataset
    need to validate that the value given (min_aX=-15.00, estimated from 2451_Shooting.csv) works for more datasets (eg 4455_Shooting.csv)

--- data-splitter.py
+++ data-splitter.py
@@ -1,104 +1,115 @@
 import pandas as pd
 import numpy as np
+import csv
 
 import time
-import timeit
 
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 # validates wether the position of the splitting points actually correspond to the splitting points in the original dataset (csv_file)
-def val_split_pos(split = [], np_array = np.array, collumn = 0):
-    start = time.perf_counter()
+def val_split_pos(split = [], np_array = np.array, collumn = 0, benchmark = False):
+    if benchmark: start = time.perf_counter()
     i=0
     for row,value in split:
         if not (value == np_array[row][collumn]):
             return False
 
-    print("valsplitpos time: {}".format(time.perf_counter()-start))
+    if benchmark: print("valsplitpos time: {}".format(time.perf_counter()-start))
     return True
 
 # searches for multiple peaks at the same points to avoid too small splits
-def del_val_multiple(split):
-    start = time.perf_counter()
+def del_val_multiple(split, benchmark = False):
+    if benchmark: start = time.perf_counter()
     single_vals = []
     for i in range(len(split)-1):
         if not (split[i][1] == split[(i+1)][1]):
             single_vals.append(split[i][0])
-    print("delvalmult t: {}".format(time.perf_counter()-start))
+    if benchmark: print("delvalmult t: {}".format(time.perf_counter()-start))
     return single_vals
 
 # splits the dataset at a givin value, no smoothening or anything, you have to applay del_va_multiple to have correct results
-def normal_splitting(collumn = 0, min_aX = -15.00, splits = [], np_array = np.array):
-    start = time.perf_counter()
+def normal_splitting(collumn = 0, min_aX = -15.00, np_array = np.array, splits = [], benchmark = False):
+    if benchmark: start = time.perf_counter()
     row = 0
     while(len(np_array) > row):
         if(np_array[row][collumn] < min_aX):
             splits.append([row,np_array[row][collumn]])
         row += 1
 
-    print("normalsplittimg t: {}".format(time.perf_counter()-start))
+    if benchmark: print("normalsplittimg t: {}".format(time.perf_counter()-start))
     return del_val_multiple(splits)
 
-def split_data(np_array = np.array, splits = []):
-    start = time.perf_counter()
-    # data_splitted = np.array([])
-    data_splitted = np.empty((0,len(splits)))
-    print(data_splitted)
-    old = 0
+def split_data_iterative(np_array = np.array, splits = [], benchmark = False):
+    if benchmark: start = time.perf_counter()
 
-    for i in range(len(splits)):
-        tmp = np_array[old: splits[i]].copy()
-        print("tmp: {}".format(tmp))
-        data_splitted = np.append(data_splitted, [tmp], axis=1)
-
-        print("data_splitted: {}".format(data_splitted[i]))
-        old = splits[i]
-        print("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \n")
-
-    print("splitdata t: {}".format(time.perf_counter()-start))
-    return data_splitted
-
-def split_data_ez(np_array = np.array, splits = []):
-    start = time.perf_counter()
     old = 0
     splitted = []
 
     for i in range(len(splits)):
         tmp = np_array[old:splits[i]].copy()
         splitted.append(tmp)
-    print("splitdataez: {}".format(time.perf_counter()-start))
-
+        old = splits[i]
+    
+    if benchmark: print("splitdataez: {}".format(time.perf_counter()-start))
     return splitted
+    
+def save_dataset(filename = "",splitted_dataset = [], header = ["aX","aY","aZ"], benchmark = False):
+    if benchmark: start = time.perf_counter()
+    i = 0
+    for data in splitted_dataset:
+        with open("splitted_data/{}_{}".format(filename, i), 'w', encoding='utf-8', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(header)
+            writer.writerows(data)
 
+        i += 1
+    if benchmark: print("save_dataset t: {}".format(time.perf_counter()-start))
+
+
+
+def show_dataset(splitted_dataset = [],benchmark = False):
+    if benchmark: start = time.perf_counter()
+    print("Found Datasets: {}".format(len(splitted_dataset)))
+
+    fig, axs = plt.subplots(len(splitted_dataset), sharex=True, sharey=True)
+    for i in range(len(splitted_dataset)):
+        axs[i].plot(splitted_dataset[i])
+        axs[i].set_title("splitted_dataset {}".format(i))
+    plt.show()
+    if benchmark: print("show_dataset t: {}".format(time.perf_counter()-start))
 
 ##################################################
-##########THE REAL PROGRAMM ######################
+########## THE REAL PROGRAMM #####################
 ##################################################
 
-print(" Started")
-path =  'Data/2451_Shooting.csv'
+path =  'data/2451_Shooting.csv'
 csv_file = pd.read_csv(path)
 np_array = csv_file.to_numpy()
-# print(np_array)
 
 min_aX = -15.00
 collumn = 0
-splits = []
 normalized_splits = []
 
-normalized_splits = normal_splitting(collumn, min_aX, splits, np_array)
+normalized_splits = normal_splitting(collumn, min_aX, np_array, benchmark=True)
+splitted_dataset = split_data_iterative(np_array, normalized_splits, benchmark=True)
 
-# print("all: {} \n normalized: {} ".format(splits, normalized_splits))
-print("\n##############################################################################\n")
+# show_dataset(splitted_dataset, benchmark=True)
+save_dataset("4455", splitted_dataset, benchmark=True)
 
-split_data_ez(np_array, normalized_splits)
+####################################################
+print("########### ANOTHER FILE ####################")
+####################################################
 
-#print("0 ::: {}".format(splitted_dataset[0][0]))
-#print("1 ::: {}".format(splitted_dataset))
+path =  'data/4455_Shooting.csv'
+csv_file = pd.read_csv(path)
+np_array = csv_file.to_numpy()
 
+min_aX = -15.00
+collumn = 0
+normalized_splits = []
 
+normalized_splits = normal_splitting(collumn, min_aX, np_array, benchmark=True)
+splitted_dataset = split_data_iterative(np_array, normalized_splits, benchmark=True)
 
-#for i in range(len(splitted_dataset)):
-#    plt.plot(splitted_dataset[i])
-#    plt.ylabel("splitted_datasert {}".format(i))
-#    plt.show()
+# show_dataset(splitted_dataset, benchmark=True)
+save_dataset("4455", splitted_dataset, benchmark=True)
